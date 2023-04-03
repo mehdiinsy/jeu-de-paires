90d73916356b4b8bbb7f3a15857338209724b1ad
mehdiinsy <126080136+mehdiinsy@users.noreply.github.com>
2023-04-03 14:54:16 +0200

    commit 4

--- paires.js
+++ paires.js
@@ -1,10 +1,12 @@
 const cards = document.querySelectorAll(".card")
-
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockboard = false;
 
 function flipCard() {
-    this.classList.toggle("flip");
+    if (lockboard) return;
+    if (this===firstCard) return;
+    this.classList.add("flip");
     // first click
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -25,10 +27,53 @@ function flipCard() {
         setTimeout(() => {
              firstCard.classList.remove("flip");
              secondCard.classList.remove("flip");
-        },1500) 
+        },500);
+       return;
+    };
+    secondCard = this;
+    checkForMatch();
+}}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+    }
+    else {
+        // not a match
+        unflipCards();
        
     }
-}}
+}
+
+
+function disableCards() {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+        resetBoard();
+}
+
+function unflipCards() {
+    lockboard = true;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+             secondCard.classList.remove("flip");
+             resetBoard();
+    },1500 );
+
+    function resetBoard() {
+        [hasFlippedCard.lockboard] = [false, false];
+        firstCard, secondCard = [null, null];
+    }
+             
+
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random()*20);
+        card.style.order = randomPos ;
+    });
+})();
 
 cards.forEach(card => card.addEventListener("click",flipCard))
 
