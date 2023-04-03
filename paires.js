b8cbaf3a850bef6c10e49f1dab46b29c1631842d
mehdiinsy <126080136+mehdiinsy@users.noreply.github.com>
2023-04-03 13:33:27 +0200

    commit 3

--- paires.js
+++ paires.js
@@ -5,11 +5,30 @@ let firstCard, secondCard;
 
 function flipCard() {
     this.classList.toggle("flip");
+    // first click
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
     }
-}
+    else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+    // les cartes marchent ?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    }
+    else {
+        // not a match
+        setTimeout(() => {
+             firstCard.classList.remove("flip");
+             secondCard.classList.remove("flip");
+        },1500) 
+       
+    }
+}}
 
 cards.forEach(card => card.addEventListener("click",flipCard))
 
