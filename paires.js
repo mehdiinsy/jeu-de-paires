f8c5e55b5de336b08e2d6f1a80328c6500e4cff3
mehdiinsy <126080136+mehdiinsy@users.noreply.github.com>
2023-04-05 14:27:39 +0200

    5

--- paires.js
+++ paires.js
@@ -1,79 +1,84 @@
-const cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
-let firstCard, secondCard;
 let lockboard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    if (lockboard) return;
-    if (this===firstCard) return;
-    this.classList.add("flip");
-    // first click
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    }
-    else {
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
+  if (lockboard) return;
+  if (this === firstCard) return;
+  this.classList.toggle("flip");
+
+  // first click
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
 
     // les cartes marchent ?
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
+      // ca match
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+    } else {
+      // not a match
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+      }, 500);
+      return;
     }
-    else {
-        // not a match
-        setTimeout(() => {
-             firstCard.classList.remove("flip");
-             secondCard.classList.remove("flip");
-        },500);
-       return;
-    };
     secondCard = this;
     checkForMatch();
-}}
-
-function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();
-    }
-    else {
-        // not a match
-        unflipCards();
-       
-    }
+  }
 }
 
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+  } else {
+    // not a match
+    unflipCards();
+  }
+}
 
 function disableCards() {
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
-        resetBoard();
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
 }
 
 function unflipCards() {
-    lockboard = true;
-    setTimeout(() => {
-        firstCard.classList.remove("flip");
-             secondCard.classList.remove("flip");
-             resetBoard();
-    },1500 );
+  lockboard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
 
-    function resetBoard() {
-        [hasFlippedCard.lockboard] = [false, false];
-        firstCard, secondCard = [null, null];
-    }
-             
-
+  function resetBoard() {
+    [hasFlippedCard, lockboard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
 }
 
 (function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random()*20);
-        card.style.order = randomPos ;
-    });
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 20);
+    card.style.order = randomPos;
+  });
 })();
 
-cards.forEach(card => card.addEventListener("click",flipCard))
+// afficher homer apres 1 minute
 
+let hp = document.createElement("img");
+hp.src = "img/homer-perdu.gif";
+const hope = document.querySelector(".memory-game");
+
+setTimeout(() => {
+  hope.appendChild(hp);
+}, 60000);
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
